d09b0f11a0c7955d4c1c4d7c33c200217b58f558
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-07-31 11:35:09 +0200

    changed logging

--- main.go
+++ main.go
@@ -13,24 +13,25 @@ import (
 func main() {
 	utils.LoadEnvs()
 
-	fmt.Printf("%v Started the Script pre-scheduler \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
+	fmt.Printf("%v: Started the Script pre-scheduler \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 	err := utils.HandleAllItemsListOfIngredients()
 	if err != nil {
 		teams_notifier.SendUnknownErrorToTeams(err)
 	}
-	fmt.Printf("%v Success \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 
-	fmt.Println("Starting the Cron Scheduler at:" + time.Now().UTC().Format("2006-01-02 15:04:05"))
+	fmt.Printf("%v: Success \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
+	fmt.Printf("%v: Started the Cron Scheduler", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
 	s := gocron.NewScheduler(time.UTC)
 	_, _ = s.Cron("0 4 * * *").SingletonMode().Do(func() {
 
-		fmt.Printf("%v Started the Script \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
+		fmt.Printf("%v: Started the Script \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 		err := utils.HandleAllItemsListOfIngredients()
 		if err != nil {
 			teams_notifier.SendUnknownErrorToTeams(err)
 		}
 		sap_api_wrapper.SapApiPostLogout()
-		fmt.Printf("%v Success \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
+		fmt.Printf("%v: Success \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 
 	})
 	s.StartBlocking()
